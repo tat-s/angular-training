af3ecbea10e6bcf61f2148bf2970b86fb9970ab4
tatsuya.sakai <tatsuya_s103@outlook.jp>
2025-01-04 14:14:01 +0900

    テキストを入力したタスク一覧に反映する

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-todo',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.scss',
 })
-export class TodoComponent {}
+export class TodoComponent {
+  todos: string[] = [];
+  inputText = '';
+
+  onClickAddTodoButton() {
+    // Todoリストに追加
+    this.todos.push(this.inputText);
+    // 入力欄をクリア
+    this.inputText = '';
+  }
+}
