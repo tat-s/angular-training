b118ba4ae66120b02337d2c6c394d65138e24f5f
tatsuya.sakai <tatsuya_s103@outlook.jp>
2025-01-07 13:53:44 +0900

    SSR系の機能を削除、OverlayScrollbarsを追加

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -2,17 +2,44 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButton, MatMiniFabButton } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { ScrollbarsAutoHideBehavior } from 'overlayscrollbars';
+import { OverlayscrollbarsModule } from 'overlayscrollbars-ngx';
 
 @Component({
   selector: 'app-todo',
-  imports: [FormsModule, MatButton, MatIconModule, MatMiniFabButton],
+  imports: [
+    FormsModule,
+    MatButton,
+    MatIconModule,
+    MatMiniFabButton,
+    OverlayscrollbarsModule,
+  ],
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.scss',
 })
 export class TodoComponent {
-  todos: string[] = []; // Todoリスト
+  todos: string[] = [
+    'AAA',
+    'BBB',
+    'CCC',
+    'DDD',
+    'EEE',
+    'FFF',
+    'GGG',
+    'HHH',
+    'III',
+  ]; // Todoリスト
   completedTodos: string[] = []; // 完了済みTodoリスト
   inputText = ''; // 入力欄のテキスト
+
+  // スクロールバーを設定
+  options = {
+    scrollbars: {
+      theme: 'os-theme-dark',
+      autoHide: 'leave' as ScrollbarsAutoHideBehavior,
+      autoHideDelay: 100,
+    },
+  };
 
   /**
    * ADDボタンがクリックされたときの処理
